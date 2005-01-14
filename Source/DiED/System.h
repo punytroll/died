6d468042392250f61923ace3612e1de6e27917d5
punytroll <punytroll@localhost>
2005-01-14 20:23:01 +0000

    Changed the vector of client to a clientid_t indexed map.

--- Source/DiED/System.h
+++ Source/DiED/System.h
@@ -1,6 +1,8 @@
 #ifndef DIED_SYSTEM_H
 #define DIED_SYSTEM_H
 
+#include <map>
+
 #include "ClientFactory.h"
 #include "ExternalEnvironment.h"
 #include "MessageFactory.h"
@@ -13,9 +15,9 @@ namespace DiED
 	public:
 		System(void);
 		virtual ~System(void);
+		void vSetClientFactory(boost::shared_ptr< DiED::ClientFactory > ClientFactory);
 		void vSetExternalEnvironment(DiED::ExternalEnvironment * pExternalEnvironment);
 		virtual boost::shared_ptr< Network::MessageFactory > GetMessageFactory(void);
-		DiED::Server & GetServer(void);
 		
 		// functions which will setup the server or the connection to a network
 		bool bListen(const Network::port_t & ServicePort);
@@ -25,6 +27,9 @@ namespace DiED
 		void vInput(const Glib::ustring & sString);
 		// implementation of the InternalEnvironment interface
 		virtual void vInsertText(DiED::User & User, const Glib::ustring & sString);
+		
+		// signals
+		sigc::signal< void, DiED::Client & > ClientConnected;
 	protected:
 		void vSendMessage(Network::BasicMessage & Message);
 		void vInsertText(DiED::User & User, const Glib::ustring & sString, bool bWriteToEnvironment);
@@ -34,7 +39,7 @@ namespace DiED
 		boost::shared_ptr< DiED::ClientFactory > m_ClientFactory;
 		boost::shared_ptr< DiED::Client > m_Client;
 		DiED::Server m_Server;
-		std::vector< boost::shared_ptr< Network::Socket > > m_Clients;
+		std::map< DiED::clientid_t, boost::shared_ptr< DiED::Client > > m_Clients;
 		DiED::ExternalEnvironment * m_pExternalEnvironment;
 		Network::port_t m_ServicePort;
 	};
