5cb03abed9839c5c941a74709cfd559c50e7000c
Hagen Möbius <hagen.moebius@googlemail.com>
2005-01-15 06:18:27 +0000

    Added simple OnMessage... handler.

--- Source/GUI/Client.cxx
+++ Source/GUI/Client.cxx
@@ -1,15 +1,21 @@
 #include "Client.h"
 
+#include <sstream>
+
+#include <DiED/BasicMessage.h>
+
 GUI::Client::Client(DiED::InternalEnvironment & InternalEnvironment) :
 	DiED::Client(InternalEnvironment),
-	m_bHoldMessagesBack(false)
+	m_bHoldMessagesBack(false),
+	m_MessageListStore(GUI::MessageListStore::create())
 {
 //~ 	std::cout << "[GUI/Client]: Created new Client." << std::endl;
 }
 
 GUI::Client::Client(int iSocket, DiED::InternalEnvironment & InternalEnvironment) :
 	DiED::Client(iSocket, InternalEnvironment),
-	m_bHoldMessagesBack(false)
+	m_bHoldMessagesBack(false),
+	m_MessageListStore(GUI::MessageListStore::create())
 {
 //~ 	std::cout << "[GUI/Client]: Created new Client from socket." << std::endl;
 }
@@ -19,10 +25,33 @@ GUI::Client::~Client(void)
 	std::cout << "[GUI/Client]: Deleted Client." << std::endl;
 }
 
-void GUI::Client::vMessageReady(void)
+Glib::RefPtr< GUI::MessageListStore > GUI::Client::GetMessageListStore(void)
 {
+	return m_MessageListStore;
+}
+
+void GUI::Client::vOnMessageReady(void)
+{
+	std::cout << "Message ready:\n\t" << boost::dynamic_pointer_cast< DiED::BasicMessage >(back())->sGetString() << std::endl;
 	if(m_bHoldMessagesBack == false)
 	{
-		DiED::Client::vMessageReady();
+		DiED::Client::vOnMessageReady();
 	}
+}
+
+void GUI::Client::vOnMessageBegin(void)
+{
+	Gtk::TreeRow Row(*m_MessageListStore->append());
+	std::stringstream ssName;
+	
+	ssName << "Type = " << back()->GetType();
+	Row[m_MessageListStore->Columns.Name] = ssName.str();
+	Row[m_MessageListStore->Columns.ClientID] = GetClientID();
+	std::cout << "Message begin: " << ssName.str() << std::endl;
+	DiED::Client::vOnMessageBegin();
+}
+
+void GUI::Client::vOnMessageExecuted(void)
+{
+	std::cout << "Message executed:\n\t" << boost::dynamic_pointer_cast< DiED::BasicMessage >(back())->sGetString() << std::endl;
 }
--- Source/GUI/Client.h
+++ Source/GUI/Client.h
@@ -13,8 +13,12 @@ namespace GUI
 		Client(DiED::InternalEnvironment & InternalEnvironment);
 		Client(int iSocket, DiED::InternalEnvironment & InternalEnvironment);
 		virtual ~Client(void);
+		
+		Glib::RefPtr< GUI::MessageListStore > GUI::Client::GetMessageListStore(void);
 	protected:
-		virtual void vMessageReady(void);
+		virtual void vOnMessageReady(void);
+		virtual void vOnMessageBegin(void);
+		virtual void vOnMessageExecuted(void);
 	private:
 		bool m_bHoldMessagesBack;
 		Glib::RefPtr< GUI::MessageListStore > m_MessageListStore;
