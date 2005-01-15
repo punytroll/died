99da854eb39d304a30d6ea946eef21a58e11b16f
Hagen Möbius <hagen.moebius@googlemail.com>
2005-01-15 16:06:46 +0000

    Added ClientMessageLists.

--- Source/GUI/MainWindow.h
+++ Source/GUI/MainWindow.h
@@ -1,6 +1,8 @@
 #ifndef GUI_MAINWINDOW_H
 #define GUI_MAINWINDOW_H
 
+#include <gtkmm/notebook.h>
+#include <gtkmm/paned.h>
 #include <gtkmm/textview.h>
 #include <gtkmm/window.h>
 
@@ -14,11 +16,18 @@ namespace GUI
 		MainWindow(DiED::System & System);
 	protected:
 		void vInserted(const Gtk::TextBuffer::iterator & Iterator, const Glib::ustring & sString, int);
+		bool bKeyPressed(GdkEventKey * pEvent);
+		
+		// implementation of DiED::ExternalEnvironment
+		virtual void vClientConnected(DiED::Client & Client);
 		virtual void vInsertText(const Glib::ustring & sString, int iLine, int iCharacter);
 	private:
 		DiED::System & m_System;
+		Gtk::VPaned m_Pane;
+		Gtk::Notebook m_Notebook;
 		Gtk::TextView m_TextView;
 		Glib::RefPtr< Gtk::TextBuffer > m_TextBuffer;
+		sigc::connection m_KeyPressedConnection;
 		sigc::connection m_InsertConnection;
 	};
 }
