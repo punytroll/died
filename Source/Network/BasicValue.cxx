67d024d638dc3752136be2687a8245c197dcc3fb
Hagen Möbius <hagen.moebius@googlemail.com>
2005-01-19 17:35:35 +0000

    Removed the sGetString function which only retrieved a mangled version of the type in an Value. Now every value has an operator<<() function which prints the value of the Value to an ostream.

--- Source/Network/BasicValue.cxx
+++ Source/Network/BasicValue.cxx
@@ -1,5 +1,10 @@
 #include "BasicValue.h"
 
+std::ostream & Network::BasicValue::operator<<(std::ostream & OStream)
+{
+	return OStream << "<Unknown Type>";
+}
+
 void Network::BasicValue::vSetReady(bool bReady)
 {
 	m_bReady = bReady;
--- Source/Network/BasicValue.h
+++ Source/Network/BasicValue.h
@@ -27,13 +27,9 @@ namespace Network
 			return m_bReady;
 		}
 		
-		virtual Glib::ustring sGetString(void) const
-		{
-			return "undefined";
-		}
-		
 		virtual void vReadFrom(boost::shared_ptr< Network::BasicReader > Reader) = 0;
 		virtual void vWriteTo(boost::shared_ptr< Network::BasicWriter > Writer) const = 0;
+		virtual std::ostream & operator<<(std::ostream & OStream);
 		
 		sigc::signal< void > Ready;
 	protected:
