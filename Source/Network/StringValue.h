adff3d41bc5b88fc1275045c21167e1a428aa026
punytroll <punytroll@localhost>
2005-01-25 17:18:12 +0000

    Fixed header file defines. Removed wrong operator<<. Moved implementation details to the C++ source file.

--- Source/Network/StringValue.h
+++ Source/Network/StringValue.h
@@ -1,5 +1,5 @@
-#ifndef STRINGVALUE_H
-#define STRINGVALUE_H
+#ifndef NETWORK_STRINGVALUE_H
+#define NETWORK_STRINGVALUE_H
 
 #include "BasicValue.h"
 
@@ -11,11 +11,10 @@ namespace Network
 		StringValue(void);
 		StringValue(const Glib::ustring & Value);
 		virtual size_t stGetSize(void) const;
-		operator const Glib::ustring &(void) const;
-		const Glib::ustring & Get(void) const;
 		virtual void vReadFrom(boost::shared_ptr< Network::BasicReader > Reader);
 		virtual void vWriteTo(boost::shared_ptr< Network::BasicWriter > Writer) const;
-		virtual std::ostream & operator<<(std::ostream & OStream);
+		operator const Glib::ustring &(void) const;
+		const Glib::ustring & Get(void) const;
 	protected:
 		Glib::ustring m_String;
 	};
